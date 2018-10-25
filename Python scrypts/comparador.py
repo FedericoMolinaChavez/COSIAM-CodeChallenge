e2f49f5209860ce82900503cd0d477e08e23d8f2
Federico Molina Chavez <federicomolinachavez@gmail.com>
2018-10-25 10:15:33 -0500

    cambios en scrypt

--- Python scrypts/comparador.py
+++ Python scrypts/comparador.py
@@ -1,3 +1,8 @@
 #Scrypt para encontrar patrones en el consumo energetico en Corinto y compararlos con los valores promedios de los otros departamentos.
 import pandas as pd
-c = pd.readcsv("../comparador.py")
+
+c = pd.read_csv("./Electrificacion.csv")
+#print(c.head())
+
+for i,row in c.iterrows():
+	print(i,row)
