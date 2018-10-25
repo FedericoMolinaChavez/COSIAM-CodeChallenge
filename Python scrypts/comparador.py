d60ef2c859090b9c17442a331f17cd232eb70a3a
Federico Molina Chavez <federicomolinachavez@gmail.com>
2018-10-25 10:44:51 -0500

    revision de valores medios

--- Python scrypts/comparador.py
+++ Python scrypts/comparador.py
@@ -5,14 +5,18 @@ c = pd.read_csv("./Electrificacion.csv")
 #print(c.head())
 
 meanUsers = c["Total suscriptores urbanos"].mean()
-meanUsage = c["Consumo promedio urbano oficial"].mean()
+meanUsage = c["Consumo total rural estrato 01"].mean()
 
-print(meanUsers)
-print(meanUsage)
+#print(meanUsers)
+#print(meanUsage)
 
 CorintoRow = c[c.Municipio == "Corinto"]
 #print(CorintoRow)
 meanUsersR = CorintoRow["Total suscriptores urbanos"].mean()
-meanUsageR = CorintoRow["Consumo promedio urbano oficial"].mean()
-print(meanUsersR)
-print(meanUsageR)
+meanUsageR = CorintoRow["Consumo total rural estrato 01"].mean()
+#print(meanUsersR)
+#print(meanUsageR)
+differenceUsage = meanUsageR-meanUsage
+differenceHabitants = meanUsersR-meanUsers
+print(differenceUsage)
+print(differenceHabitants)
